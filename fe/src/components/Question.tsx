ee381cce52a1ee68e651ddac4cdaa5a16ae06348
Ozan Akın <ozn.akn@gmail.com>
2023-02-07 04:59:45 +0300

    updates

--- fe/src/components/Question.tsx
+++ fe/src/components/Question.tsx
@@ -44,7 +44,7 @@ export default function Question({ paths }: { paths: string[] }) {
   ) : (
     <Box>
       <Box sx={{ textAlign: 'center', display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center' }}>
-        <Typography variant="h2">
+        <Typography variant="h4">
           {selectedNode.text}
         </Typography>
 
@@ -54,7 +54,7 @@ export default function Question({ paths }: { paths: string[] }) {
               key={`button-${i}`}
               variant="contained"
               size="large"
-              sx={{ m: 2, minWidth: '400px' }}
+              sx={{ m: 2, minWidth: '120px' }}
               onClick={() => {
                 if (location.pathname === '/') {
                   navigate(`/${i}`);
