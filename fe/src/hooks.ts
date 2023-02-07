ec69fd959c1a1865e07bb68b8b6b1d2f1006742a
Ozan Akın <ozn.akn@gmail.com>
2023-02-07 14:29:32 +0300

    upd

--- fe/src/hooks.ts
+++ fe/src/hooks.ts
@@ -5,7 +5,7 @@ import { useQuery } from "react-query";
 
 import { TreeNodeType } from "./variables/TreeNode";
 
-const baseQuestionData = axios.get(`https://raw.githubusercontent.com/alpaylan/afetbilgi.com/main/data/all.json?v=3`)
+const baseQuestionData = axios.get(`https://raw.githubusercontent.com/alpaylan/afetbilgi.com/main/data/all.json?v=3.1`)
   .then(res => res.data);
 
 const baseQuestionDataEN = axios.get(`https://raw.githubusercontent.com/alpaylan/afetbilgi.com/main/data/all.en.json?v=3`)
