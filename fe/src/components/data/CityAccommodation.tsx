b3482052e29772a17ec4ed364ea971ba0dbd3a0b
alpaylan <alpkeles99@gmail.com>
2023-02-07 00:31:41 -0500

    update city accomodation

--- fe/src/components/data/CityAccommodation.tsx
+++ fe/src/components/data/CityAccommodation.tsx
@@ -4,11 +4,15 @@ import { CityAccommodationNode } from "../../interfaces/TreeNode";
 export default function CityAccommodation({ value }: { value: CityAccommodationNode }) {
   return (
     <Box>
-      <h3>{value.city} Şehrine Kalabileceğiniz Yerler</h3>
+      <h3>{value.city} Şehrinde Kalabileceğiniz Yerler</h3>
+
+      <p><b>Bu sayfada, içerisinde belirtilenler hariç tüm yerler telefonla doğrulanmıştır. Ancak günler, hatta saatler içerisinde bu bilgiler değişebildiğinden dolayı, kendi araştırmanızı yapmanız önemle rica edilir.</b></p>
+
       {value.items.map((item) => (
         <Paper sx={{ p: 2, m: 2 }}>
-          <Box>{item.name}</Box>
-          <a href={item.url} target="_blank" rel="noreferrer">{item.url}</a>
+          <b>{item.name}</b> depremzedelere kalacak yer sağladığını bildirdi. 
+          Detaylı bilgiye <a href={item.url} target="_blank" rel="noreferrer">bu linkten</a> ulaşabilirsiniz.
+          {(!item.is_validated && <p><b>Not: Bu bilgiyi telefonla doğrulayamadık.</b></p>)}
         </Paper>
       ))}
     </Box>
