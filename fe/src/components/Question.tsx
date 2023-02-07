61d2e9e341e820b2cbb09ebe34b0cbe49906ba4e
erenerisken <erenerisken@gmail.com>
2023-02-07 12:17:35 +0300

    Add autocomplete dropdown for questions having autocompleteHint field set

--- fe/src/components/Question.tsx
+++ fe/src/components/Question.tsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-restricted-syntax */
 
-import { Box, Button, Typography } from '@mui/material';
+import {
+  Autocomplete,
+  Box,
+  Button,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
-import {useQuestionData } from '../hooks';
+import { useQuestionData } from '../hooks';
 import { TreeNodeType } from '../variables/TreeNode';
 import Data from './Data';
+import { OptionNode } from '../interfaces/TreeNode';
 
 export default function Question({ paths }: { paths: string[] }) {
   const location = useLocation();
@@ -17,34 +24,79 @@ export default function Question({ paths }: { paths: string[] }) {
   }
 
   return selectedNode.type !== TreeNodeType.NODE_TYPE_QUESTION ? (
-    <Data dataNode={(selectedNode as any)} />
+    <Data dataNode={selectedNode as any} />
   ) : (
     <Box>
-      <Box sx={{ textAlign: 'center', display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center' }}>
-        <Typography variant="h4">
-          {selectedNode.text}
-        </Typography>
+      <Box
+        sx={{
+          textAlign: 'center',
+          display: 'flex',
+          flexFlow: 'column nowrap',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant='h4'>{selectedNode.text}</Typography>
 
-        <Box sx={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'center', paddingBottom: '50px' }}>
-          {selectedNode.options.map((option: any) => (
-            <Button
-              key={`button-${option.name}`}
-              variant="contained"
-              size="medium"
-              sx={{ m: 2, minWidth: '300px' }}
-              onClick={() => {
+        <Box
+          sx={{
+            display: 'flex',
+            flexFlow: 'row wrap',
+            justifyContent: 'center',
+            paddingBottom: '50px',
+          }}
+        >
+          {selectedNode.autocompleteHint ? (
+            <Autocomplete
+              id='options-autocomplete'
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label={selectedNode.autocompleteHint}
+                  variant='outlined'
+                  sx={{ minWidth: '200px', m: 2 }}
+                />
+              )}
+              options={selectedNode.options}
+              getOptionLabel={(option: OptionNode) => option.name}
+              onChange={(_, selectedOption) => {
+                if (!selectedOption) {
+                  return;
+                }
+
                 if (location.pathname === '/') {
-                  navigate(`/${encodeURIComponent(option.name)}`);
+                  navigate(`/${encodeURIComponent(selectedOption.name)}`);
                 } else {
-                  navigate(`${location.pathname}/${encodeURIComponent(option.name)}`);
+                  navigate(
+                    `${location.pathname}/${encodeURIComponent(
+                      selectedOption.name,
+                    )}`,
+                  );
                 }
               }}
-            >
-              {option.name}
-            </Button>
-          ))}
+            />
+          ) : (
+            selectedNode.options.map((option: any) => (
+              <Button
+                key={`button-${option.name}`}
+                variant='contained'
+                size='medium'
+                sx={{ m: 2, minWidth: '300px' }}
+                onClick={() => {
+                  if (location.pathname === '/') {
+                    navigate(`/${encodeURIComponent(option.name)}`);
+                  } else {
+                    navigate(
+                      `${location.pathname}/${encodeURIComponent(option.name)}`,
+                    );
+                  }
+                }}
+              >
+                {option.name}
+              </Button>
+            ))
+          )}
         </Box>
       </Box>
     </Box>
   );
-};
+}
--- fe/src/interfaces/Common.ts
+++
@@ -1,3 +0,0 @@
-export interface PropsWithPath {
-  path: string;
-}
