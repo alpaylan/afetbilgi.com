f2a55c40063b566d06ebad8c53cff2968087976c
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-07 16:41:25 +0300

    update data

--- fe/src/components/data/PhoneData.tsx
+++ fe/src/components/data/PhoneData.tsx
@@ -1,13 +1,33 @@
-import { ListItem, Box, List } from "@mui/material";
+import { Box, Paper, Table, TableContainer, TableHead, TableRow,TableCell, TableBody } from "@mui/material";
 import { TelephoneDataNode } from "../../interfaces/TreeNode";
 
 export default function TelephoneData({ value }: { value: TelephoneDataNode }) {
-  return (
-    <Box>
-      <h3>Önemli Telefon Numaraları</h3>
-      <List>
-        {value.phones.map((v, i)=> <ListItem key={`item-${i}`}>{v.name}: <b><a href={v.is_plain ? `tel:${v.phone_number}` : `tel:+90${v.phone_number.replace(/^0/, "").replace(/ /g, "")}`}>{v.phone_number}</a></b></ListItem>)}
-      </List>
-    </Box>
-  )
+    return (
+        <Box>
+            <h3>Önemli Telefon Numaraları</h3>
+            <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
+                <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Birim</TableCell>
+                        <TableCell>Telefon&nbsp;(g)</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {value.phones.map((item) => (
+                        <TableRow
+                        key={item.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {item.name}
+                        </TableCell>
+                        <TableCell><a href={item.is_plain ? `tel:${item.phone_number}` : `tel:+90${item.phone_number.replace(/^0/, "").replace(/ /g, "")}`}>{item.phone_number}</a></TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+      );
 }
