06f75f855b28f65307b97e86f9e08c5a64221c73
Ozan Akın <ozn.akn@gmail.com>
2023-02-07 11:05:32 +0300

    some ui fixes

--- fe/src/components/data/CreditCardData.tsx
+++ fe/src/components/data/CreditCardData.tsx
@@ -5,7 +5,8 @@ export default function CreditCardData({ value }: { value: CreditCardNode }) {
   return (
     <Box>
         <Paper sx={{ p: 2, m: 2 }}>
-        <b>{value.name}</b> kredi kartı yoluyla bağış yapılmasına izin vermektedir. 
+          <b>{value.name}</b> kredi kartı yoluyla bağış yapılmasına izin vermektedir.
+          &nbsp;
           <a href={value.url} target="_blank" rel="noreferrer">bu linke</a> tıklayarak ilgili sayfaya ulaşabilir ve bağışınızı gerçekleştirebilirsiniz.
         </Paper>
     </Box>
