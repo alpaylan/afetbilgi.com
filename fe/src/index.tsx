5f5c0740fb9f7caed330f9c3daa9259793d92d9e
Furkan Dogan <furkandoganktf@hotmail.com>
2023-02-08 01:29:19 +0300

    Feat: Add multi language support

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -11,6 +11,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
+import './i18n';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
