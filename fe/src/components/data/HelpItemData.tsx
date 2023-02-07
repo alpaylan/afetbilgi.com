cfec7439a4f48e02ec2395bbfe82116a376bdd7c
Ozan Akın <ozn.akn@gmail.com>
2023-02-07 14:20:08 +0300

    fix

--- fe/src/components/data/HelpItemData.tsx
+++ fe/src/components/data/HelpItemData.tsx
@@ -7,7 +7,7 @@ export default function HelpItemData({ value }: { value: HelpItemNode }) {
       <h3>{value.city} Eşya Yardım Alanları</h3>
       {value.items.map((item, i) => (
         <Paper key={`help-item-${i}`} sx={{ m: 2, p: 2 }}>
-          <p><b>{item.name}</b>'in {(item.location && <b>{item.location}</b>)}'de düzenlediği eşya yardımı kampanyasına yardım etmek için <a href={item.url} target="_blank" rel="noreferrer">bu linke</a> tıklayınız.</p>
+          <p><b>{item.name}</b>'in {(item.location && <span><b>{item.location}</b>'de</span>)} düzenlediği eşya yardımı kampanyasına yardım etmek için <a href={item.url} target="_blank" rel="noreferrer">bu linke</a> tıklayınız.</p>
           {(item.phone_number && <p>Detaylı bilgi için <b>{item.phone_number}</b>'i arayabilirsiniz.</p>)}
         </Paper>
       ))}
