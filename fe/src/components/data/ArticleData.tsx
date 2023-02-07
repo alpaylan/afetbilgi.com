d138c6a7d44fb915969d4ac9cd77abf1e5cbf9ce
Yigit Varli <yigit.varli0505@gmail.com>
2023-02-07 20:19:32 +0000

    update page vıew

--- fe/src/components/data/ArticleData.tsx
+++ fe/src/components/data/ArticleData.tsx
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, Paper } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import { ArticleDataNode } from "../../interfaces/TreeNode";
 
 export default function ArticleData({ value }: { value: ArticleDataNode }) {
@@ -7,17 +7,24 @@ export default function ArticleData({ value }: { value: ArticleDataNode }) {
             <h3>Faydalı Yazılar</h3>
 
             <p><b>Deprem sonrası ile alakalı faydalı yazıları aşağıda bulabilirsiniz.</b></p>
-    
-            {value.articles.map((item, i) => (
 
-            <Paper sx={{ p: 2, m: 2 }} key={`item-${i}`}>
-                <List>
-                    <ListItem><b>Başlık: </b>{item.title}</ListItem>
-                    <ListItem><b>Yazar: </b>{item.author}</ListItem>
-                    <ListItem><b>Link: </b><a href={item.url} target="_blank">{item.url}</a></ListItem>
-                </List>
-            </Paper>
+            {value.articles.map((item, i) => (
+                <Paper sx={{ p: 2, m: 2 }} key={`item-${i}`}>
+                <b>{item.title}</b>
+                <br /> <br />
+                <b>Yazar: </b> {item.author}
+                <br /><br />
+
+                {item.url && (
+                    <>
+                    Yazıya <a href={item.url} target="_blank" rel="noreferrer">bu linkten</a> ulaşabilirsiniz.
+                    <br />
+                    </>
+                )}
+
+                <br />
+                </Paper>
             ))}
         </Box>
-    );
+      );
 }
