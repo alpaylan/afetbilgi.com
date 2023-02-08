aaf9437937109de472ac0f654be8317dc7f992c2
koybasimuhittin <koybasimuhittin@gmail.com>
2023-02-08 20:26:12 +0300

    fix: undefined objects

--- pdfConverterAPI/safeGatheringPlaces.js
+++ pdfConverterAPI/safeGatheringPlaces.js
@@ -12,12 +12,19 @@ const createSafeGatheringPlacePDF = (doc, data, city) => {
     const pageHeight = doc.internal.pageSize.height
 
     const cityObj = getSafeGatheringPlace(data, city)
-    const cityName = cityObj.name_tr
+
+    if(!cityObj) {
+        return;
+    }
+    if(city != 'Adana') {
+        doc.addPage();
+    }
+
+    const cityName = cityObj?.name_tr
 
     setFont(doc, "bold")
     doc.setFontSize(18)
     doc.text(`${cityName} - Güvenli Toplanma Alanları`, 16, 24)
-
     setFont(doc, "regular")
     doc.setFontSize(8)
     doc.text(`Dosyanın oluşturulma tarihi: ${getTime()}`, 16, 36)
@@ -25,7 +32,7 @@ const createSafeGatheringPlacePDF = (doc, data, city) => {
     let isNewPage = true
     let y = 60
     doc.setFontSize(8)
-    cityObj.value.data.items.forEach((el, index) => {
+    cityObj?.value.data.items.forEach((el, index) => {
         if (y >= pageHeight) {
             doc.addPage();
             isNewPage = true
