07380c28a6f298ed712649e085ff24b4a31936e2
Yigit Varli <yigit.varli0505@gmail.com>
2023-02-08 17:30:16 +0000

    change ordering

--- data/combine.py
+++ data/combine.py
@@ -3,16 +3,16 @@ import json
 data_points = [
   { 'name_tr': 'Geçici Barınma Alanları', 'name_en': 'Temporary Accommodation Places', 'path': 'datasets/barinma.json' },
   { 'name_tr': 'Güvenli Toplanma Alanları', 'name_en': 'Safe Gathering Places', 'path': 'datasets/toplanma.json' },
-#   { 'name_tr': 'Para Bağışı İmkanları', 'name_en': 'Money Donation', 'path': 'datasets/bagis.json' },
-  { 'name_tr': 'Yardım Toplama Merkezleri','name_en': 'Other Donation',  'path': 'datasets/yardim_toplama_merkezleri.json' },
-  { 'name_tr': 'Kızılay Kan Bağış Noktaları', 'name_en': 'Kızılay Blood Donation Places', 'path': 'datasets/blood.json' },
+  { 'name': 'Yemek', 'name_en': 'Food', 'path': 'datasets/yemek.json' },
   { 'name': 'Önemli Telefon Numaraları', 'name_en': 'Crucial Phone Number', 'path': 'datasets/telefon.json' },
   { 'name': 'Önemli Web Siteleri', 'name_en': 'Useful Links', 'path': 'datasets/faydali_linkler.json' },
-  { 'name': 'Faydalı Yazılar', 'name_en': 'Useful Articles' , 'path': 'datasets/yazilar.json' },
-  { 'name': 'Kök Hücre Bağış Noktaları', 'name_en': 'Stem Cell Donation Points', 'path': 'datasets/kokhucre.json' },
   { 'name': 'Veterinerler', 'name_en': 'Veterinarians', 'path': 'datasets/veteriner.json' },
-  { 'name': 'Yemek', 'name_en': 'Food', 'path': 'datasets/yemek.json' },
+  { 'name_tr': 'Yardım Toplama Merkezleri','name_en': 'Other Donation',  'path': 'datasets/yardim_toplama_merkezleri.json' },
+  { 'name_tr': 'Kızılay Kan Bağış Noktaları', 'name_en': 'Kızılay Blood Donation Places', 'path': 'datasets/blood.json' },
+  { 'name': 'Kök Hücre Bağış Noktaları', 'name_en': 'Stem Cell Donation Points', 'path': 'datasets/kokhucre.json' },
+  { 'name': 'Faydalı Yazılar', 'name_en': 'Useful Articles' , 'path': 'datasets/yazilar.json' },
   { 'name': 'VPN', 'path': 'datasets/vpn.json' },
+#   { 'name_tr': 'Para Bağışı İmkanları', 'name_en': 'Money Donation', 'path': 'datasets/bagis.json' },
 ]
 
 result = {
