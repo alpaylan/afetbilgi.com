2899b7822ffb00697d5bddc79427f681ab36a91b
Paralaks <61003980+onuradali@users.noreply.github.com>
2023-02-08 17:46:19 +0300

    Toplanma Alanları Hızlı Konum Erişim
    Toplanma alanları hızlı konum erişimi için konum bağlantısı eklendi.

--- fe/src/components/data/GatheringData.tsx
+++ fe/src/components/data/GatheringData.tsx
@@ -1,4 +1,13 @@
-import { ListItem, Box, List } from '@mui/material';
+import {
+  Box,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableContainer
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { GatheringDataNode } from '../../interfaces/TreeNode';
 
@@ -6,16 +15,40 @@ export default function GatheringData({ value }: { value: GatheringDataNode }) {
   const { t } = useTranslation();
   return (
     <Box>
-      <h3>
-        {t('data.gathering.title', {
+        <h3>{t('data.gathering.title', {
           city: value.city,
-        })}
-      </h3>
-      <List>
-        {value.items.map((v, i) => (
-          <ListItem key={`item-${i}`}>{v}</ListItem>
-        ))}
-      </List>
+        })}</h3>
+        <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
+          <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Toplanma Alanı</TableCell>
+                <TableCell>Konum</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {value.items.map((v) => (
+                <TableRow
+                  key={v}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {v}
+                  </TableCell>
+                  <TableCell>
+                    <a
+                      href={`https://maps.google.com/?q=${v
+                        .replace('(', '')
+                        .replace(')', '')} ${value.city}`}
+                    >
+                      Konum
+                    </a>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
     </Box>
   );
 }
