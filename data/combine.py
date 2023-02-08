e7af6b3d4e23084ce0185936daf0eb08aa24f55a
Ozan Akın <ozn.akn@gmail.com>
2023-02-08 18:58:36 +0300

    updates

--- data/combine.py
+++ data/combine.py
@@ -1,16 +1,16 @@
 import json
 
 data_points = [
-  { 'name_tr': 'Geçici Barınma Alanları', 'name_en': 'Temporary Accommodation Places', 'path': 'datasets/barinma.json' },
-  { 'name_tr': 'Güvenli Toplanma Alanları', 'name_en': 'Safe Gathering Places', 'path': 'datasets/toplanma.json' },
+  { 'name_tr': 'Geçici Barınma Alanları', 'name_en': 'Temporary Accommodation Places', 'name_ku': 'Bicîhbûna Demkî', 'path': 'datasets/barinma.json' },
+  { 'name_tr': 'Güvenli Toplanma Alanları', 'name_en': 'Safe Gathering Places', 'name_ar': 'Qadên Ewle Bo Kombûnê', 'path': 'datasets/toplanma.json' },
 #   { 'name_tr': 'Para Bağışı İmkanları', 'name_en': 'Money Donation', 'path': 'datasets/bagis.json' },
-  { 'name_tr': 'Yardım Toplama Merkezleri','name_en': 'Other Donation',  'path': 'datasets/yardim_toplama_merkezleri.json' },
-  { 'name_tr': 'Kızılay Kan Bağış Noktaları', 'name_en': 'Kızılay Blood Donation Places', 'path': 'datasets/blood.json' },
-  { 'name': 'Önemli Telefon Numaraları', 'name_en': 'Crucial Phone Number', 'path': 'datasets/telefon.json' },
-  { 'name': 'Önemli Web Siteleri', 'name_en': 'Useful Links', 'path': 'datasets/faydali_linkler.json' },
-  { 'name': 'Faydalı Yazılar', 'name_en': 'Useful Articles' , 'path': 'datasets/yazilar.json' },
-  { 'name': 'Kök Hücre Bağış Noktaları', 'name_en': 'Stem Cell Donation Points', 'path': 'datasets/kokhucre.json' },
-  { 'name': 'Veterinerler', 'name_en': 'Veterinarians', 'path': 'datasets/veteriner.json' },
+  { 'name_tr': 'Yardım Toplama Merkezleri','name_en': 'Other Donation', 'name_ar': 'Bexşkirina Tiştan', 'path': 'datasets/yardim_toplama_merkezleri.json' },
+  { 'name_tr': 'Kızılay Kan Bağış Noktaları', 'name_en': 'Kızılay Blood Donation Places', 'name_ar': 'Cihên Xwîn Dayînê yên Kizilay/ Heyva Sor', 'path': 'datasets/blood.json' },
+  { 'name': 'Önemli Telefon Numaraları', 'name_en': 'Crucial Phone Number', 'name_ar': 'Numareyên Girîng', 'path': 'datasets/telefon.json' },
+  { 'name': 'Önemli Web Siteleri', 'name_en': 'Useful Links', 'name_ar': ' Malperên Kêrhatî', 'path': 'datasets/faydali_linkler.json' },
+  { 'name': 'Faydalı Yazılar', 'name_en': 'Useful Articles' , 'name_ar': 'Agahiyên Kêrhatî', 'path': 'datasets/yazilar.json' },
+  { 'name': 'Kök Hücre Bağış Noktaları', 'name_en': 'Stem Cell Donation Points', 'name_ar': 'XZY', 'path': 'datasets/kokhucre.json' },
+  { 'name': 'Veterinerler', 'name_en': 'Veterinarians', 'name_ar': 'Veterîner', 'path': 'datasets/veteriner.json' },
   { 'name': 'Yemek', 'name_en': 'Food', 'path': 'datasets/yemek.json' },
 ]
 
@@ -18,8 +18,8 @@ result = {
     'type': 'question',
     'text_tr': 'Lütfen bilgi almak istediğiniz konuyu seçiniz.',
     'text_en': 'Please select a topic.',
-    'text_ku': '',
-    'text_at': '',
+    'text_ku': 'i kerema xwe mijara hûn dixwazin agahdariyê jê bigirin hilbijêrin',
+    'text_ar': 'الرجاء تحديد الموضوع الذي تريد تلقي معلومات عنه',
     'options': []
 }
 
