c3079bcbe4f29b6aee4d8cbba5db2790a397b060
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2023-02-08 16:18:00 +0300

    Add functionality for creating safe gathering places

--- pdfConverterAPI/pdfOperations.js
+++ pdfConverterAPI/pdfOperations.js
@@ -1,24 +1,62 @@
 const { default: axios } = require("axios");
 const { jsPDF } = require("jspdf")
 const myFont = require("./fonts/Roboto-Black-normal")
+const SGP = require("./safeGatheringPlaces")
 
 const DATA_URL = "https://raw.githubusercontent.com/alpaylan/afetbilgi.com/main/data/all.combined.1.json";
 
-const setFont = (doc) => {
+const registerFont = (doc) => {
     doc.addFileToVFS("./fonts/Roboto-Black.ttf", myFont.font);
     doc.addFont("./fonts/Roboto-Black.ttf", "Roboto-Black", "normal");
-    doc.setFont('Roboto-Black', 'normal');
+    doc.addFont("./fonts/Roboto-Regular.ttf", "Roboto-Regular", "normal");
+    doc.setFont('Roboto-Regular', 'normal');
+}
+
+const setFont = (doc, type) => {
+    if (type == "regular") {
+        doc.setFont('Roboto-Regular', 'normal');
+    } else {
+        doc.setFont('Roboto-Black', 'normal');
+    }
 }
 
 const createPDF = async () => {
-    const doc = new jsPDF()
     const data = await fetchData();
+    createSafeGatheringPlacePDF(data, "Malatya")
+}
 
-    setFont(doc)
+const createSafeGatheringPlacePDF = (data, city) => {
+    const doc = new jsPDF({
+        orientation: "p",
+        unit: "px",
+        format: "a4"
+    })
+    registerFont(doc)
+    const pageHeight = doc.internal.pageSize.height
 
-    doc.text("bçşğüa", 10, 10)
-    doc.save("b.pdf")
+    const cityObj = SGP.getSafeGatheringPlace(data, city)
+    const cityName = cityObj.name_tr
 
+    setFont(doc, "bold")
+    doc.setFontSize(18)
+    doc.text(cityName + " - " + "Güvenli Toplanma Alanları", 16, 24)
+
+    setFont(doc, "regular")
+    doc.setFontSize(8)
+    doc.text("Dosyanın oluşturulma tarihi: " + getTime(), 16, 36)
+
+    let y = 60
+    doc.setFontSize(8)
+    cityObj.value_tr.data.items.forEach((el, index) => {
+        if (y >= pageHeight) {
+            doc.addPage();
+            y = 12
+        }
+        doc.text(el, 16, y)  
+        y += 12
+    });
+
+    doc.save(cityName + ".pdf")
 }
 
 //fetches data
@@ -29,6 +67,16 @@ const fetchData = async () => {
 }
 createPDF()
 
+const getTime = () => {
+    const date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes();
+
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+
+    return hours + ":" + minutes;
+}
 
 module.exports = {
     createPDF,
--- pdfConverterAPI/safeGatheringPlaces.js
+++ pdfConverterAPI/safeGatheringPlaces.js
@@ -1,5 +1,5 @@
 // gets safe gathering place data of given city
-const getSafeGatheringPlace = async (data, city) => {
+const getSafeGatheringPlace = (data, city) => {
     const safeGatheringPlaceData = data.options.filter(op => op.name_tr == "Güvenli Toplanma Alanları")
     const cityData = safeGatheringPlaceData[0].value.options.filter(op => op.name_tr == city)
     return cityData[0];
