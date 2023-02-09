676273f5f3f8e7db206b38d07c067b8a26f219b3
R. Kürşat Vuruşan <kursat@pm.me>
2023-02-09 03:37:39 +0300

    Kokhucre parser tamamlandi

--- data/parsers/kokhucre_parser.py
+++ data/parsers/kokhucre_parser.py
@@ -6,7 +6,6 @@ if __name__ == '__main__':
     sheet_id = "136czRg-KSQ4zW_1rP1vwJpMFi57GeDeN_0Wh-bFNCjw"
     sheet_name = "K%C3%B6k%20H%C3%BCcre%20Ba%C4%9F%C4%B1%C5%9F%20Noktalar%C4%B1"
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-    print(url)
     df = pd.read_csv(url)
     df.rename(columns={'BÖLGE İSMİ':'area', 'ŞEHİR':'city', 'ADRES':'address', 'TELEFON NUMARASI':'phone'}, inplace=True)
     json_obj = {}
