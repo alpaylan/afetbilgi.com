025dc87be86da55fcc1a947531929121ed20161d
ayberkgokmen <ayberk.gokmen@gmail.com>
2023-02-09 14:08:23 +0300

    tabular ui in useful links

--- fe/src/components/data/UsefulLinksdata.tsx
+++ fe/src/components/data/UsefulLinksdata.tsx
@@ -1,56 +1,65 @@
-import { Box, Paper } from '@mui/material';
+import {
+  Box,
+  Paper,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { UsefulLink, UsefulLinksDataNode } from '../../interfaces/TreeNode';
+import { UsefulLinksDataNode } from '../../interfaces/TreeNode';
 import { Language } from '../../utils/types';
 
-const UsefulLinksLanguageHelper = ({
-  item,
-  index,
-}: {
-  item: UsefulLink;
-  index: number;
-}) => {
-  const { i18n } = useTranslation();
-  if (i18n.language === Language.TR) {
-    return (
-      <Paper sx={{ p: 2, m: 2 }} key={`item-${index}`}>
-        <b>{item.name}</b>
-        <br />
+// const UsefulLinksLanguageHelper = ({
+//   item,
+//   index,
+// }: {
+//   item: UsefulLink;
+//   index: number;
+// }) => {
+//   const { i18n } = useTranslation();
+//   if (i18n.language === Language.TR) {
+//     return (
+//       <Paper sx={{ p: 2, m: 2 }} key={`item-${index}`}>
+//         <b>{item.name}</b>
+//         <br />
 
-        {item.url && (
-          <>
-            Sayfaya{' '}
-            <a href={item.url} target='_blank' rel='noreferrer'>
-              bu linkten
-            </a>{' '}
-            ulaşabilirsiniz.
-            <br />
-          </>
-        )}
+//         {item.url && (
+//           <>
+//             Sayfaya{' '}
+//             <a href={item.url} target='_blank' rel='noreferrer'>
+//               bu linkten
+//             </a>{' '}
+//             ulaşabilirsiniz.
+//             <br />
+//           </>
+//         )}
 
-        <br />
-      </Paper>
-    );
-  }
-  return (
-    <Paper sx={{ p: 2, m: 2 }} key={`item-${index}`}>
-      <b>{item.name}</b>
-      <br />
+//         <br />
+//       </Paper>
+//     );
+//   }
+//   return (
+//     <Paper sx={{ p: 2, m: 2 }} key={`item-${index}`}>
+//       <b>{item.name}</b>
+//       <br />
 
-      {item.url && (
-        <>
-          You can reach the page from{' '}
-          <a href={item.url} target='_blank' rel='noreferrer'>
-            this link
-          </a>
-          <br />
-        </>
-      )}
+//       {item.url && (
+//         <>
+//           You can reach the page from{' '}
+//           <a href={item.url} target='_blank' rel='noreferrer'>
+//             this link
+//           </a>
+//           <br />
+//         </>
+//       )}
 
-      <br />
-    </Paper>
-  );
-};
+//       <br />
+//     </Paper>
+//   );
+// };
 
 export default function UsefulLinksData({
   value,
@@ -59,7 +68,9 @@ export default function UsefulLinksData({
   value: UsefulLinksDataNode;
   noTitle?: boolean;
 }) {
-  const { t } = useTranslation();
+  const { t, i18n} = useTranslation();
+
+  
   return (
     <Box>
       {!noTitle && (
@@ -71,9 +82,51 @@ export default function UsefulLinksData({
           </p>
         </>
       )}
-      {value.usefulLinks.map((item, i) => (
-        <UsefulLinksLanguageHelper key={i} item={item} index={i} />
-      ))}
+      <TableContainer component={Paper} >
+        <Table >
+          <TableHead>
+            <TableRow>
+              <TableCell>{t('name')}</TableCell>
+              <TableCell>{t('details')}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {value.usefulLinks.map((item, i) => (
+            <TableRow
+              key={i}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                {item.name}
+              </TableCell>
+              <TableCell>
+                <>
+                  {i18n.language !== Language.TR &&  item.url && (
+                    <>
+                      You can reach the page from{' '}
+                      <a href={item.url} target='_blank' rel='noreferrer'>
+                        this link
+                      </a>
+                      <br />
+                    </>
+                  )}
+                  {i18n.language === Language.TR && item.url && (
+                    <>
+                      Sayfaya{' '}
+                      <a href={item.url} target='_blank' rel='noreferrer'>
+                        bu linkten
+                      </a>{' '}
+                      ulaşabilirsiniz.
+                      <br />
+                    </>
+                  )}
+                </>
+              </TableCell>
+            </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 }
