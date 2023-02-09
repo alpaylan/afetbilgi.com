f09f2d8d9b859979551ee0ead9f7f8722bbcfcb9
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-09 21:12:56 +0300

    update parsers/faydali_linkler

--- data/parsers/faydalilinkler.py
+++ data/parsers/faydalilinkler.py
@@ -1,13 +1,19 @@
-import pandas as pd
+import sys
 import json
+import pandas as pd
 
 def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} <output-file>")
+        sys.exit(1)
+
+    out_path = sys.argv[1]
+
     sheet_id = "136czRg-KSQ4zW_1rP1vwJpMFi57GeDeN_0Wh-bFNCjw"
     sheet_name = "%C3%96nemli%20Web%20Siteleri"
 
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-    json_name = "../datasets/faydali_linkler.json"
     df = pd.read_csv(url, encoding="utf-8", header=None)
 
     usefulLinks = []
@@ -35,7 +41,7 @@ def main():
         }
     }
 
-    with open(json_name, "w+", encoding="utf-8") as f:
+    with open(out_path, "w+", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
