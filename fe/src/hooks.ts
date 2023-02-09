90162961075960e263758cd79dcd3a4c8ffb4b57
Ozan Akın <ozn.akn@gmail.com>
2023-02-09 22:42:10 +0300

    upd

--- fe/src/hooks.ts
+++ fe/src/hooks.ts
@@ -6,7 +6,7 @@ import { useQuery } from 'react-query';
 
 import { TreeNodeType } from './variables/TreeNode';
 
-const baseQuestionData = axios.get(`https://cdn.afetbilgi.com/latest.json?v=2.2`)
+const baseQuestionData = axios.get(`https://cdn.afetbilgi.com/latest.json?v=2.3`)
   .then(res => res.data);
 
 export const useQuestionData = (paths: string[]) => {
