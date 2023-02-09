9d8eeb4e756cb7c5331cdad80831f9363164f569
Ozan Akın <ozn.akn@gmail.com>
2023-02-10 02:22:58 +0300

    add google maps

--- maps/src/Map.tsx
+++ maps/src/Map.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import ReactLeafletGoogleLayer from 'react-leaflet-google-layer';
+import { MapContainer, Marker, Popup } from 'react-leaflet';
 import { useMarkers } from './hooks';
+
+const MAP_API_KEY = 'AIzaSyDUCSBvWKR3TXtogQD2YNsIUA66Jg_BC1k';
 
 export default function Map() {
   const markers = useMarkers();
 
   return (
     <MapContainer center={[37.569442, 37.190833]} zoom={8} scrollWheelZoom={false} style={{ height: '100vh' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <ReactLeafletGoogleLayer apiKey={MAP_API_KEY} />
+
       {markers.map((marker, i) => (
         <Marker key={`marker-${i}`} position={[marker.lat, marker.lng]}>
-          <Popup>Selam</Popup>
+          <Popup>
+            {marker.name}
+            <p>{marker.description}</p>
+            <div>{marker.url && <a href={marker.url} target="_blank">{marker.url}</a>}</div>
+          </Popup>
         </Marker>
       ))}
     </MapContainer>
--- maps/src/hooks.ts
+++ maps/src/hooks.ts
@@ -2,7 +2,10 @@ export const useMarkers = () => {
   return [
     {
       lng: 37.569442,
-      lat: 37.190833
+      lat: 37.190833,
+      name: 'Test',
+      description: 'Desc',
+      url: 'https://google.com',
     }
   ]
 }
