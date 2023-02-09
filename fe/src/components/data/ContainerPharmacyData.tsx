77bffad564d7e836d7cbdd19d458b42fcebaaeb0
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-09 18:43:15 +0300

    update (#70)

--- fe/src/components/data/ContainerPharmacyData.tsx
+++ fe/src/components/data/ContainerPharmacyData.tsx
@@ -1,4 +1,4 @@
-import { Paper, Box, TableContainer, Table, TableRow, TableCell, TableHead, TableBody } from '@mui/material';
+import { Paper, Box, TableContainer, Table, TableRow, TableCell, CardContent, Card, TableHead, TableBody, useMediaQuery, List, ListItem } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { ContainerPharmacyDataNode } from '../../interfaces/TreeNode';
 
@@ -8,11 +8,30 @@ export default function ContainerPharmacyData({
   value: ContainerPharmacyDataNode;
 }) {
   const { t } = useTranslation();
+  const isMinWidth = useMediaQuery('(max-width:600px)');
   return (
     <Box>
       <h3>{t('data.container_pharmacy')}</h3>
-      <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
-        <Table sx={{ maxWidth: 650 }}>
+      {isMinWidth ? <List>
+        {value.items.map((item) => (
+          <ListItem>
+            <Card sx={{width: "100%"}}>
+              <CardContent>
+                <b>{t('city')}</b>: {item.city}<br/><br/>
+                <b>{t('district')}</b>: {item.district}<br/><br/>
+                <b>{t('location')}</b>: {item.location}<br/><br/>
+                <b>{t('map')}</b>: <a
+                        href={item.locationLink}
+                        target="_blank"
+                      >
+                        Konum
+                      </a><br/>
+              </CardContent>
+            </Card>
+          </ListItem>
+        ))}
+        </List> : <TableContainer component={Paper} sx={{ maxWidth: 650, minWidth: 100 }}>
+        <Table sx={{ maxWidth: 650, minWidth: 100 }}>
           <TableHead>
             <TableRow>
               <TableCell>{t('city')}</TableCell>
@@ -42,7 +61,7 @@ export default function ContainerPharmacyData({
             ))}
           </TableBody>
         </Table>
-      </TableContainer>
+      </TableContainer>}
     </Box>
   );
 }
