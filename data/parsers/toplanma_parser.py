6c582608c4031ade83e6e1dcc363afe8b8c46999
Ozan Akın <ozn.akn@gmail.com>
2023-02-09 22:07:40 +0300

    update toplanma parser

--- data/parsers/toplanma_parser.py
+++ data/parsers/toplanma_parser.py
@@ -87,6 +87,7 @@ def main():
                     "type": "data",
                     "data": {
                         "dataType": "new-gathering-list",
+                        "city": city_name,
                         "items": items
                     }
                 }
