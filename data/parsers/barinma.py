c401ff8cd4b0c48a99e7da3d359074a8f173a844
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-09 21:10:39 +0300

    update parsers/barinma

--- data/parsers/barinma.py
+++ data/parsers/barinma.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import pandas as pd
 
@@ -11,13 +12,18 @@ def index_or_none(l, i):
         return None
 
 def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} <output-file>")
+        sys.exit(1)
+
+    out_path = sys.argv[1]
+
     city_translation = json.loads(open("./utils/il_translate.json").read())
 
     sheet_id = "131Wi8A__gpRobBT3ikt5VD3rSZIPZxxtbqZTOUHUmB8"
     sheet_name = "Ge%C3%A7ici%20Bar%C4%B1nma%20Alanlar%C4%B1"
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-    json_name = "../datasets/barinma.json"
     df = pd.read_csv(url, encoding="utf-8")
 
     parsed = []
@@ -118,7 +124,7 @@ def main():
         }
     }
 
-    with open(json_name, "w+", encoding="utf-8") as f:
+    with open(out_path, "w+", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
