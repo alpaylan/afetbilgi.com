789a6af5ae0119e65624b377cded4704efb42203
R. Kürşat Vuruşan <kursat@pm.me>
2023-02-10 05:28:25 +0300

    download button links fixed

--- fe/src/utils/downloadPDF.ts
+++ fe/src/utils/downloadPDF.ts
@@ -1,3 +1,3 @@
 export const downloadPDF = (pathname: string) => {
-  window.open(`https://cdn.afetbilgi.com${pathname}`, '_blank');
+  window.open(`https://cdn.afetbilgi.com/pdfs${pathname === '/' ? '/AfetBilgi' : decodeURI(pathname)}.pdf`, '_blank');
 };
