3e8d43dfcf3cff9d598ada6f46582da409051089
Kaanayden <kaanayden15@gmail.com>
2023-02-10 14:04:08 +0300

    Add filter multiple types

--- maps/src/Test.tsx
+++ maps/src/Test.tsx
@@ -1,4 +1,4 @@
-import { filterType } from "./helpers/filters";
+import { filterMultipleTypes, filterType } from "./helpers/filters";
 import { useMarkers } from "./hooks";
 
 export default function Test() {
@@ -6,6 +6,9 @@ export default function Test() {
     const data = useMarkers()
     const filteredMapData = filterType(data.map_data, "city-accommodation | gathering-list | help-item-list | phone-number-list | useful-links | beneficial-articles | stem-cell-donation | data-vet | food-items");
     console.log(filteredMapData)
+
+    const filteredMapDataTypes = filterMultipleTypes(data.map_data, ["test", "city-accommodation | gathering-list | help-item-list | phone-number-list | useful-links | beneficial-articles | stem-cell-donation | data-vet | food-items"],);
+    console.log(filteredMapDataTypes)
 
     return(<></>)
 }
--- maps/src/helpers/filters.ts
+++ maps/src/helpers/filters.ts
@@ -6,6 +6,19 @@ export const filterType = (data: MarkerData["map_data"], type: string) =>  {
     return data.filter (item => isType(item, type))
 }
 
+export const filterMultipleTypes = (data: MarkerData["map_data"], types: string[]) =>  {
+    return data.filter (item => isOneOfTypes(item, types))
+}
+
+const isOneOfTypes = (data : MarkerData["map_data"][0], types: string[]) => {
+    for(let i = 0; i < types.length; i+=1) {
+        if( data.type === types[i] ) {
+            return true;
+        }
+    }
+    return false; 
+
+}
 
 const isType = (data : MarkerData["map_data"][0], type: string) => {
     return type === data.type
