48d65af1aa1f33567cd8f7d1c8b11bc5af3e9c0b
Kaanayden <kaanayden15@gmail.com>
2023-02-10 13:23:42 +0300

    Start map filtering

---
+++ maps/src/helpers/filters.ts
@@ -0,0 +1,20 @@
+import { MapDataItem, MarkerData } from "../hooks";
+
+
+export const filterType
+
+
+const isType = (data : MarkerData["map_data"][], type: string) {
+    data
+
+}
+
+export const searchNames = ( data: MarkerData, searchName: string ): MarkerData => {
+
+    const filteredData = data.map_data.data.filter( item =>  )
+
+} 
+
+const doesExistName = ( data : MapDataItem, searchName: string ): boolean => {
+
+}
