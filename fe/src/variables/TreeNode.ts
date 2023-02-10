baaa17d1c9bed5fc0572ae071330f75757f4975d
Yigit Varli <yigit.varli0505@gmail.com>
2023-02-10 17:30:04 +0000

    conflict

--- fe/src/variables/TreeNode.ts
+++ fe/src/variables/TreeNode.ts
@@ -30,11 +30,8 @@ export enum DataType {
   VPN = 'vpn',
   CONTAINER_PHARMACY = 'container-pharmacy',
   EVACUATION_POINTS = 'evacuation-points',
-<<<<<<< HEAD
   DONATION_LINKS = 'donation-links',
-=======
   TRANSPORTATIONS = 'transportations',
->>>>>>> eb65dbc407e78c7b257566bcbb31686c73e76dbd
 }
 
 export const dataTypeToCategoryMap: { [key in DataType as string]: Category } =
