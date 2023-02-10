c7cbf316ca8bc253af6ee446a53556cc580e11b5
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-10 03:20:32 +0300

    workflow

--- pdfConverterAPI/runner.py
+++ pdfConverterAPI/runner.py
@@ -2,7 +2,7 @@ import os
 from PyPDF2 import PdfMerger
 
 OUTPUT_ROOT = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "outputs")
+    os.path.realpath(__file__)), "AfetBilgi")
 
 
 def generateParentPDF(folder):
