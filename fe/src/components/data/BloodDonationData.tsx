ec1f7f2fa07f279a08736ff9af048dd561ded35c
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-10 12:34:21 +0300

    home nav

--- fe/src/components/data/BloodDonationData.tsx
+++ fe/src/components/data/BloodDonationData.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function BloodDonationData() {
+  const navigate = useNavigate();
   window.open('https://www.kanver.org/KanHizmetleri/KanBagisiNoktalari', '_blank');
+  navigate("/");
   return <></>;
 }
