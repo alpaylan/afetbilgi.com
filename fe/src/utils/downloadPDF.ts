e7cb042b2636f2ade2b7ce5e68b0378e260df7d6
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-11 01:23:13 +0300

    calculated version for PDF download

--- fe/src/utils/downloadPDF.ts
+++ fe/src/utils/downloadPDF.ts
@@ -3,7 +3,9 @@ export const downloadPDF = (lang: string) => {
   // const version = d.getDate().toString().concat(".", d.getHours().toString());
   // window.open(`https://cdn.afetbilgi.com/pdfs${pathname === '/' ? '/AfetBilgi' : decodeURI(pathname)}.pdf?v=${version}`, '_blank');
 
+  const d = new Date();
+  const version = d.getDate().toString().concat(".", d.getHours().toString(), d.getMinutes().toString());
 
   // TODO: Replace this with pdf.afetbilgi.com
-  window.open(`https://cdn.afetbilgi.com/md-pdf/${lang}/afetbilgi.pdf?v=2.4`, '_blank');
+  window.open(`https://cdn.afetbilgi.com/md-pdf/${lang}/afetbilgi.pdf?v=${version}`, '_blank');
 };
