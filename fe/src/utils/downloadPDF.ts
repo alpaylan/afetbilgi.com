8103ceb0a599e3b4562371b215427ffd68c042ef
Ozan Sazak <ozan.sazak@protonmail.ch>
2023-02-11 16:11:03 +0300

    Feat: Purge Cloudflare cache after PDF generation #140

--- fe/src/utils/downloadPDF.ts
+++ fe/src/utils/downloadPDF.ts
@@ -1,11 +1,3 @@
 export const downloadPDF = (lang: string) => {
-  // const d = new Date();
-  // const version = d.getDate().toString().concat(".", d.getHours().toString());
-  // window.open(`https://cdn.afetbilgi.com/pdfs${pathname === '/' ? '/AfetBilgi' : decodeURI(pathname)}.pdf?v=${version}`, '_blank');
-
-  const d = new Date();
-  const version = d.getDate().toString().concat(".", d.getHours().toString(), d.getMinutes().toString());
-
-  // TODO: Replace this with pdf.afetbilgi.com
-  window.open(`https://cdn.afetbilgi.com/md-pdf/${lang}/afetbilgi.pdf?v=${version}`, '_blank');
+  window.open(`https://pdf.afetbilgi.com/${lang}`, '_blank');
 };
