fb8a2d4048468a7b17f16e5d4d3a446341be22cc
Ozan Akın <ozn.akn@gmail.com>
2023-02-11 12:16:57 +0300

    clean project

--- maps/src/icons.ts
+++
@@ -1,9 +0,0 @@
-import L from 'leaflet';
-
-// import markerBlue from './assets/marker-blue.png';
-import markerBlack from './assets/marker-black.png';
-
-export const iconBlack = new L.Icon({
-    iconUrl: markerBlack,
-    iconSize: new L.Point(30, 40),
-});
