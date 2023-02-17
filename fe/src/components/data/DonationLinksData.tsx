92423a13086d98e9417e036e5ac1414f7f13c227
Ozan Akın <ozn.akn@gmail.com>
2023-02-17 12:24:07 +0300

    some style fix

--- fe/src/components/data/DonationLinksData.tsx
+++ fe/src/components/data/DonationLinksData.tsx
@@ -1,52 +1,52 @@
 import {
-    Box,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-  } from '@mui/material';
-  import { useTranslation } from 'react-i18next';
-  import { DonationLinksDataNode } from '../../interfaces/TreeNode';
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { DonationLinksDataNode } from '../../interfaces/TreeNode';
 import Title from '../Title';
-  
-  export default function DonationLinksData({ value }: { value: DonationLinksDataNode }) {
-    const { t } = useTranslation();
-    return (
-      <Box>
-        <Title
-          title={t('data.donation.title')}
-          subtitle={t('data.donation.subtitle') || ""}
-          severity="success"
-        />
-        <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
-          <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
-            <TableHead>
-              <TableRow>
-                <TableCell>{t('name')}</TableCell>
-                <TableCell>{t('website')}</TableCell>
+
+export default function DonationLinksData({ value }: { value: DonationLinksDataNode }) {
+  const { t } = useTranslation();
+
+  return (
+    <Box>
+      <Title
+        title={t('data.donation.title')}
+        subtitle={t('data.donation.subtitle') || ""}
+        severity="success"
+      />
+      <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
+        <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>{t('name')}</TableCell>
+              <TableCell>{t('website')}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {value.items.map((item) => (
+              <TableRow
+                key={item.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  {item.name}
+                </TableCell>
+                <TableCell>
+                  <a href={item.url}>Link</a>
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {value.items.map((item) => (
-                <TableRow
-                  key={item.name}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component='th' scope='row'>
-                    {item.name}
-                  </TableCell>
-                  <TableCell>
-                    <a href={item.url}>Link</a>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-    );
-  }
-  
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
--- fe/src/components/data/ServicesData.tsx
+++ fe/src/components/data/ServicesData.tsx
@@ -1,83 +1,82 @@
 import {
-    Box,
-    CardContent,
-    Card,
-    List,
-    ListItem,
-    Typography,
-  } from '@mui/material';
-  import { useTranslation } from 'react-i18next';
-  import { ServicesDataNode } from '../../interfaces/TreeNode';
-  import Title from '../Title';
-  
-  export default function ServicesData({
-    value,
-  }: {
-    value: ServicesDataNode;
-  }) {
-    const { t } = useTranslation();
+  Box,
+  CardContent,
+  Card,
+  List,
+  ListItem,
+  Typography,
+} from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { ServicesDataNode } from '../../interfaces/TreeNode';
+import Title from '../Title';
 
-    return (
-      <Box>
-        <Title
-          title={t('data.services.title')}
-        />
-        <Typography variant="h6" sx={{ mt: 2 }}>
-          {t('service.category')}: {value.category}
-        </Typography>
-        <List>
-            {value.items.map((item, i) => (
-              <ListItem key={i}>
-                <Card sx={{ width: '100%' }}>
-                  <CardContent>
-                    <b>{t('city')}</b>: {item.city}
+export default function ServicesData({
+  value,
+}: {
+  value: ServicesDataNode;
+}) {
+  const { t } = useTranslation();
+
+  return (
+    <Box>
+      <Title
+        title={t('data.services.title')}
+      />
+      <Typography variant="h6" sx={{ mt: 2 }}>
+        {t('service.category')}: {value.category}
+      </Typography>
+      <List>
+          {value.items.map((item, i) => (
+            <ListItem key={i}>
+              <Card sx={{ width: '100%' }}>
+                <CardContent>
+                  <b>{t('city')}</b>: {item.city}
+                  <br />
+                  <br />
+                  <b>{t('district')}</b>: {item.county}
+                  <br />
+                  <br />
+                  <b>{t('location')}</b>: {item.location}
+                  <br />
+                  {
+                    item.specificCategory ?
+                    <>
                     <br />
+                    <b>{t('service.specific')}: </b>{' '}
+                      {item.specificCategory}
                     <br />
-                    <b>{t('district')}</b>: {item.county}
+                    </>
+                    : ''
+                  }
+                  {
+                    item.source ?
+                    <>
                     <br />
+                    <b>{t('source')}: </b>{' '}
+                    <a href={item.source} target='_blank'>
+                      {t('Link')}
+                    </a>
                     <br />
-                    <b>{t('location')}</b>: {item.location}
+                    </>
+                    : ''
+                  }
+                  {
+                    item.locationLink ?
+                    <>
                     <br />
-                    {
-                      item.specificCategory ?
-                      <>
-                      <br />
-                      <b>{t('service.specific')}: </b>{' '}
-                        {item.specificCategory}
-                      <br />
-                      </>
-                      : ''
-                    }
-                    {
-                      item.source ?
-                      <>
-                      <br />
-                      <b>{t('source')}: </b>{' '}
-                      <a href={item.source} target='_blank'>
-                        {t('Link')}
-                      </a>
-                      <br />
-                      </>
-                      : ''
-                    }
-                    {
-                      item.locationLink ?
-                      <>
-                      <br />
-                      <a href={item.locationLink} target='_blank'>
-                        {t('map')}
-                      </a>
-                      <br />
-                      </>
-                      : ''
-                    }
+                    <a href={item.locationLink} target='_blank'>
+                      {t('map')}
+                    </a>
                     <br />
-                  </CardContent>
-                </Card>
-              </ListItem>
-            ))}
-          </List>
-      </Box>
-    );
-  }
-  
+                    </>
+                    : ''
+                  }
+                  <br />
+                </CardContent>
+              </Card>
+            </ListItem>
+          ))}
+        </List>
+    </Box>
+  );
+}
