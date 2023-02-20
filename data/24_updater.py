56dcd914867373d88442312912859f27167216e0
Ozan Akın <ozn.akn@gmail.com>
2023-02-20 22:29:37 +0300

    fix

--- data/24_updater.py
+++ data/24_updater.py
@@ -15,36 +15,41 @@ def walk(options1, options2):
     if len(options1) > 0 and options1[0]['value']['type'] == 'question':
         for i in range(len(options1)):
             if 'options' in options1[i]['value'] and 'options' in options2[i]['value']:
-                walk(options1[i]['value']['options'], options2[i]['value']['options'])
+                walk(
+                    options1[i]['value']['options'] if i < len(options1) else None,
+                    options2[i]['value']['options'] if i < len(options2) else None
+                )
 
     else:
         options1_set = set()
         options2_set = set()
 
+        if options1:
+            for group1 in options1:
+                group1 = group1['value']
 
-        for group1 in options1:
-            group1 = group1['value']
+                options1_set.add(
+                    hashlib.md5(json.dumps(group1).encode('utf-8')).hexdigest()
+                )
 
-            options1_set.add(
-                hashlib.md5(json.dumps(group1).encode('utf-8')).hexdigest()
-            )
+        if options2:
+            for group2 in options2:
+                group2 = group2['value']
 
-        for group2 in options2:
-            group2 = group2['value']
+                options2_set.add(
+                    hashlib.md5(json.dumps(group2).encode('utf-8')).hexdigest()
+                )
 
-            options2_set.add(
-                hashlib.md5(json.dumps(group2).encode('utf-8')).hexdigest()
-            )
+        if options2:
+            for group2 in options2:
+                group2 = group2['value']
 
-        for group2 in options2:
-            group2 = group2['value']
+                h = hashlib.md5(json.dumps(group2).encode('utf-8')).hexdigest()
 
-            h = hashlib.md5(json.dumps(group2).encode('utf-8')).hexdigest()
-
-            if h not in options1_set:
-                group2['added_last_day'] = True
-            else:
-                group2['added_last_day'] = False
+                if h not in options1_set:
+                    group2['added_last_day'] = True
+                else:
+                    group2['added_last_day'] = False
 
 base_data = requests.get('https://cdn.afetbilgi.com/2023-02-20_16-12-37.json')
 
