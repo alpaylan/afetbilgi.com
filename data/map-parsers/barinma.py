69de74bfe33b1858929321bafa5d7065c2046cc6
Emre KÜÇÜKTEMEL <emrekucuktemel@gmail.com>
2023-02-21 02:39:15 +0300

    fix

--- data/map-parsers/barinma.py
+++ data/map-parsers/barinma.py
@@ -11,7 +11,8 @@ class BarinmaParser(BaseMapParser):
     async def parse(cls):
         sheet_id = "131Wi8A__gpRobBT3ikt5VD3rSZIPZxxtbqZTOUHUmB8"
         sheet_name = "Ge%C3%A7ici%20Bar%C4%B1nma%20Alanlar%C4%B1"
-        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+        sheet_gid = "2029961498"
+        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={sheet_gid}"
 
         df = pd.read_csv(url, encoding="utf-8")
 
