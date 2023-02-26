ded229c92358a532e5e1f0fb8802c873179f37e0
erenerisken <erenerisken@gmail.com>
2023-02-26 23:31:40 +0300

    Initialize frontend repo for admin panel

---
+++ admin-panel-fe/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import { Box } from '@mui/material';
+
+function App() {
+  return <Box component='div'>Panel</Box>;
+}
+
+export default App;
---
+++ admin-panel-fe/src/index.tsx
@@ -0,0 +1,44 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ToastContainer } from 'material-react-toastify';
+
+import 'material-react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+export const muiCache = createCache({
+  key: 'mui',
+  prepend: true,
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <CacheProvider value={muiCache}>
+    <React.StrictMode>
+      <ThemeProvider theme={createTheme()}>
+        <App />
+        <ToastContainer
+          position='top-center'
+          autoClose={6000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          draggable
+          pauseOnHover
+        />
+      </ThemeProvider>
+    </React.StrictMode>
+  </CacheProvider>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
